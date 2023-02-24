5e250625295c16b1d8c6e86d5e33c7e85e8df2ec
Alexey Belkov <lexabelco2013@yandex.ru>
2023-02-24 21:12:54 +0500

    feat: создать Main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from 'react';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 import './App.scss';
 
 const App: FunctionComponent = () => {
   return (
     <div className="app">
       <Header />
+
+      <Main>
+
+      </Main>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.scss';
 import "./vendor/normalize.scss";
 import "./vendor/fonts.scss";
 import App from './components/App/App';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
